e7602de85b7aaeeb9e83143f02e576455da94c07
K Sichien <ksichien@posteo.net>
2017-09-21 11:53:06 +0200

    added wmi script

---
+++ get-ip-mac-addresses.ps1
@@ -0,0 +1,9 @@
+$location = Read-Host -Prompt 'Please enter the searchbase' # f.e. ou=Computers,DC=example,DC=com
+$computercollection = Get-ADComputer -Filter * -SearchBase "$location"
+foreach($computer in $computercollection) {
+    $wmi = Get-WmiObject -Class "Win32_NetworkAdapterConfiguration" -ComputerName $computer.Name -Filter "IpEnabled = TRUE"
+    write-host "Connection information for " $computer.Name
+    write-host "Adapter Name:" $wmi.Description
+    write-host "MAC Address:" $wmi.MacAddress
+    write-host "IP Address:" $wmi.IPAddress
+}
