5b908a1cdb836f86c95c4532a4536a8ce4ab486f
ksichien <ksichien@posteo.net>
2018-02-12 16:33:06 +0100

    standardized format-disk scripot

--- local/format-disk.ps1
+++ local/format-disk.ps1
@@ -1,13 +1,15 @@
 #!/usr/bin/env pwsh
 
-$diskpart = 'c:\diskpart.txt'
-$drive = 'f:\'
-$files = 'c:\new folder\'
+function format-disk ([string]$diskpart, [string]$drive, [string]$files){
+    cmd /c diskpart /s $diskpart
+    copy-item -path $files -destination $drive -recurse
+    cmd /c icacls $drive /grant "Administrators:(OI)(CI)F" /t
+    cmd /c icacls $drive /remove "Everyone" /t
+    cmd /c icacls $drive /grant "Everyone:(OI)(CI)RX" /t
+}
 
-cmd /c diskpart /s $diskpart
+$example_diskpart = 'c:\diskpart.txt'
+$example_drive = 'f:\'
+$example_files = 'c:\new folder\'
 
-copy-item -path $files -destination $drive -recurse
-
-cmd /c icacls $drive /grant "Administrators:(OI)(CI)F" /t
-cmd /c icacls $drive /remove "Everyone" /t
-cmd /c icacls $drive /grant "Everyone:(OI)(CI)RX" /t
+format-disk $example_diskpart $example_drive $example_files
